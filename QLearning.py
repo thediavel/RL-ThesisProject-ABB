ceec12eec6801e4f51446d95e11a7017f4de5f81
thediavel <vshrma09@gmail.com>
2020-03-03 16:22:33 +0100

    state representation change

--- QLearning.py
+++ QLearning.py
@@ -6,14 +6,16 @@ import pickle
 import os
 import matplotlib.pyplot as plt
 import copy
-
+from datetime import datetime
 
 class qLearning:
     def __init__(self, learningRate, decayRate, numOfEpisodes, stepsPerEpisode, epsilon ,annealingConstant, annealAfter):
         self.voltageRanges=['<0.75','0.75-0-79','0.8-0-84','0.85-0.89','0.9-0.94','0.95-0.99','1-1.04','1.05-1.09','1.1-1.14','1.15-1.19','1.2-1.24','>=1.25'];
         self.voltageRanges_2=['<0.85','0.85-0.874','0.875-0.899','0.9-0.924','0.925-0-949','0.95-0.974','0.975.0.999','1-1.024','1.025-1.049','1.05-1.074','1.075-1.1','>=1.1'];
         self.loadingPercentRange=['0-9','10-19','20-29','30-39','40-49','50-59','60-69','70-79','80-89','90-99','100-109','110-119','120-129','130-139','140-149','150 and above'];
-        self.states=['v:' + x + ';l:' + y for x in self.voltageRanges_2 for y in self.loadingPercentRange]
+        self.statesLev1 = ['v_' + x + '_l_' + y for x in self.voltageRanges_2 for y in self.loadingPercentRange]
+        self.states=['s1:' + x + ';s2:' + y +';' for x in self.statesLev1 for y in self.statesLev1]
+
         self.env_2bus=powerGrid_ieee2();
         self.actions=['v_ref:'+str(x)+';lp_ref:'+str(y) for x in self.env_2bus.actionSpace['v_ref_pu'] for y in self.env_2bus.actionSpace['lp_ref']]
         self.checkPointName='pickled_q_table_lr'+str(learningRate)+'dr'+str(decayRate)+'noe'+str(numOfEpisodes)+'spe'+str(stepsPerEpisode)+'e'+str(epsilon)+'ac'+str(annealingConstant)+'aa'+str(annealAfter)+'.pkl';
@@ -35,25 +37,36 @@ class qLearning:
         self.decayRate = decayRate
         self.annealAfter=annealAfter
 
-    def getStateFromMeasurements(self, voltageLoadingPercent):
-        voltage=voltageLoadingPercent[0];
-        loadingPercent=voltageLoadingPercent[1];
-        v_ind = (math.floor(round(voltage * 100) / 5) - 14);
-        v_ind = v_ind if v_ind >=0 else 0;
-        v_ind = v_ind if v_ind < len(self.voltageRanges) else len(self.voltageRanges)-1;
-        l_ind = (math.floor(round(loadingPercent)/10));
-        l_ind = l_ind if l_ind < len(self.loadingPercentRange) else len(self.loadingPercentRange)-1;
-        return 'v:'+self.voltageRanges[v_ind]+';l:'+self.loadingPercentRange[l_ind];
+    def getStateFromMeasurements(self, voltageLoadingPercentArray):
+        res='';
+        count=1;
+        for voltageLoadingPercent in voltageLoadingPercentArray:
+            voltage=voltageLoadingPercent[0];
+            loadingPercent=voltageLoadingPercent[1];
+            v_ind = (math.floor(round(voltage * 100) / 5) - 14);
+            v_ind = v_ind if v_ind >=0 else 0;
+            v_ind = v_ind if v_ind < len(self.voltageRanges) else len(self.voltageRanges)-1;
+            l_ind = (math.floor(round(loadingPercent)/10));
+            l_ind = l_ind if l_ind < len(self.loadingPercentRange) else len(self.loadingPercentRange)-1;
+            res=res + 's'+str(count)+':v_' + self.voltageRanges[v_ind] + '_l_' + self.loadingPercentRange[l_ind]+';';
+            count+=1
+        return res;
 
-    def getStateFromMeasurements_2(self, voltageLoadingPercent):
-        voltage=voltageLoadingPercent[0];
-        loadingPercent=voltageLoadingPercent[1];
-        v_ind = (math.floor(round(voltage * 100) / 2.5) - 33);
-        v_ind = v_ind if v_ind >=0 else 0;
-        v_ind = v_ind if v_ind < len(self.voltageRanges_2) else len(self.voltageRanges_2)-1;
-        l_ind = (math.floor(round(loadingPercent)/10));
-        l_ind = l_ind if l_ind < len(self.loadingPercentRange) else len(self.loadingPercentRange)-1;
-        return 'v:'+self.voltageRanges_2[v_ind]+';l:'+self.loadingPercentRange[l_ind];
+    def getStateFromMeasurements_2(self, voltageLoadingPercentArray):
+        res = '';
+        count = 1;
+        for voltageLoadingPercent in voltageLoadingPercentArray:
+            voltage = voltageLoadingPercent[0];
+            loadingPercent = voltageLoadingPercent[1];
+            v_ind = (math.floor(round(voltage * 100) / 2.5) - 33);
+            v_ind = v_ind if v_ind >= 0 else 0;
+            v_ind = v_ind if v_ind < len(self.voltageRanges_2) else len(self.voltageRanges_2) - 1;
+            l_ind = (math.floor(round(loadingPercent) / 10));
+            l_ind = l_ind if l_ind < len(self.loadingPercentRange) else len(self.loadingPercentRange) - 1;
+            res = res + 's' + str(count) + ':v_' + self.voltageRanges_2[v_ind] + '_l_' + self.loadingPercentRange[
+                l_ind] + ';';
+            count += 1
+        return res;
 
     def getActionFromIndex(self, ind):
         actionString=self.actions[ind];
@@ -61,8 +74,6 @@ class qLearning:
         voltage = actionStringSplitted[0].split(':')[1];
         loadingPercent = actionStringSplitted[1].split(':')[1];
         return((int(loadingPercent),float(voltage) ));
-
-
 
     def test(self):
         rewards=[]
@@ -74,13 +85,15 @@ class qLearning:
                count+=1;
         print('Number of Unique States Visited: '+ str(count));
           # print(q_table[i].min())
-        for i in range(0,0):
-            currentMeasurements = self.env_2bus.getCurrentState();
-            currentState = self.getStateFromMeasurements_2(currentMeasurements);
+        currentMeasurements = self.env_2bus.getCurrentState();
+        oldMeasurements=currentMeasurements;
+        for i in range(0,12):
+            currentState = self.getStateFromMeasurements_2([oldMeasurements,currentMeasurements]);
             actionIndex = self.q_table[currentState].idxmax();
             #print(q_table[currentState].unique())
             action = self.getActionFromIndex(actionIndex);
-            nextStateMeasurements, reward, done = self.env_2bus.takeAction(action[0], action[1]);
+            oldMeasurements=currentMeasurements;
+            currentMeasurements, reward, done = self.env_2bus.takeAction(action[0], action[1]);
             print(self.env_2bus.net.res_bus.vm_pu)
             print(self.env_2bus.net.res_line)
             rewards.append(reward);
@@ -114,7 +127,7 @@ class qLearning:
             compensation.append({'k':copyNetwork.k_old,'q': copyNetwork.q_old})
             measurements.append({'v_bus1': copyNetwork.net.res_bus.vm_pu[1], 'lp_std': np.std(copyNetwork.net.res_line.loading_percent)})
         currentMeasurements = self.env_2bus.getCurrentState();
-        currentState = self.getStateFromMeasurements_2(currentMeasurements);
+        currentState = self.getStateFromMeasurements_2([currentMeasurements,currentMeasurements]);
         actionIndex = self.q_table[currentState].idxmax();
         #action = getActionFromIndex(actionIndex);
         #nextStateMeasurements2, reward2, done2 = env_2bus.takeAction(action[0], action[1]);
@@ -134,9 +147,10 @@ class qLearning:
                 accumulatedReward=0;
                 self.env_2bus.reset();
                 currentMeasurements = self.env_2bus.getCurrentState();
+                oldMeasurements = currentMeasurements;
                 for j in range(0,self.numOfSteps):
                     epsComp = np.random.random();
-                    currentState=self.getStateFromMeasurements_2(currentMeasurements);
+                    currentState=self.getStateFromMeasurements_2([oldMeasurements,currentMeasurements]);
                     if epsComp <= self.epsilon:
                         # Exploration Part
                         actionIndex = np.random.choice(99, 1)[0]
@@ -144,18 +158,19 @@ class qLearning:
                         # Greedy Approach
                         actionIndex=self.q_table[currentState].idxmax();
                     action = self.getActionFromIndex(actionIndex);
-                    nextStateMeasurements, reward, done = self.env_2bus.takeAction(action[0],action[1])
+                    oldMeasurements=currentMeasurements;
+                    currentMeasurements, reward, done = self.env_2bus.takeAction(action[0],action[1])
                     accumulatedReward += reward;
                     if done:
                         nextStateMaxQValue = 0;
                     else:
-                        nextState = self.getStateFromMeasurements_2(nextStateMeasurements);
+                        nextState = self.getStateFromMeasurements_2([oldMeasurements,currentMeasurements]);
                         nextStateMaxQValue=self.q_table[nextState].max();
                     self.q_table.iloc[actionIndex,self.states.index(currentState)] = self.q_table[currentState][actionIndex] + self.learningRate*(reward + self.decayRate*nextStateMaxQValue - self.q_table[currentState][actionIndex])
                     if done:
                         break;
                 self.allRewards.append(accumulatedReward);
-                if (i+1) %10 == 0:
+                if (i+1) %100 == 0:
                     print('Episode: '+str(i+1)+'; reward:'+str(accumulatedReward))
                 if (i+1) % self.annealAfter == 0:
                     self.epsilon=self.annealingRate*self.epsilon;
@@ -164,6 +179,6 @@ class qLearning:
                     pickle.dump(pickledData, open(self.checkPointName, "wb"))
 
 
-ql=qLearning(0.001,0.9,2000,12,1,0.98,150);
-ql.train()
+#ql=qLearning(0.001,0.9,2000,12,1,0.98,150);
+#ql.train()
 #ql.test()
--- test.py
+++ test.py
@@ -4,16 +4,26 @@ import time
 import logging
 
 ql1=qLearning(0.001,0.9,2000,12,1,0.98,150);
-ql2=qLearning(0.001,0.8,2000,12,1,0.98,150);
+#print(ql1.states[0])
+
+#print(ql1.q_table['s1:v_1-1.024_l_0-9;s2:v_0.95-0.974_l_20-29;']);
+
+#print(ql1.getStateFromMeasurements_2([(0.85,9),(0.95,9)]))
+#ql2=qLearning(0.001,0.8,2000,12,1,0.98,150);
+#ql3=qLearning(0.001,0.7,2000,12,1,0.98,150);
 ql1.train();
+#ql1.test()
 # try:
 #     threads = []
 #     x = threading.Thread(target=ql1.train)
 #     threads.append(x)
 #     x.start();
-#     y = threading.Thread(target=ql2.train)
-#     threads.append(y)
-#     y.start();
+#     # y = threading.Thread(target=ql2.train)
+#     # threads.append(y)
+#     # y.start();
+#     # y = threading.Thread(target=ql3.train)
+#     # threads.append(y)
+#     # y.start();
 #     for index, thread in enumerate(threads):
 #         logging.info("Main    : before joining thread %d.", index)
 #         thread.join()
