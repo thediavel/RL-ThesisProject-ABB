09759441b3bad02f9a1cc4336dd5a52835b347b5
Vishnu <vshrma09@gmail.com>
2020-03-09 17:11:30 +0100

    ignore

--- test.py
+++ test.py
@@ -5,26 +5,31 @@ import logging
 
 
 ############## done training ################
-
 #ql1=qLearning(0.001,0.9,20100,12,1,0.98,150);
 #ql2=qLearning(learningRate=0.001,decayRate=0.9,numOfEpisodes=24000,stepsPerEpisode=12,epsilon=1,annealingConstant=0.98,annealAfter=200);
-#ql2=qLearning(learningRate=0.001,decayRate=0.9,numOfEpisodes=30000,stepsPerEpisode=12,epsilon=1,annealingConstant=0.98,annealAfter=250);
-#ql2=qLearning(learningRate=0.001,decayRate=0.7,numOfEpisodes=20100,stepsPerEpisode=12,epsilon=1,annealingConstant=0.98,annealAfter=150);
-#ql2=qLearning(learningRate=0.001,decayRate=0.7,numOfEpisodes=24000,stepsPerEpisode=12,epsilon=1,annealingConstant=0.98,annealAfter=200);
+#ql3=qLearning(learningRate=0.001,decayRate=0.9,numOfEpisodes=30000,stepsPerEpisode=12,epsilon=1,annealingConstant=0.98,annealAfter=250);
+#ql4=qLearning(learningRate=0.001,decayRate=0.7,numOfEpisodes=20100,stepsPerEpisode=12,epsilon=1,annealingConstant=0.98,annealAfter=150);
+#ql5=qLearning(learningRate=0.001,decayRate=0.7,numOfEpisodes=24000,stepsPerEpisode=12,epsilon=1,annealingConstant=0.98,annealAfter=200);
+#ql6=qLearning(learningRate=0.001,decayRate=0.7,numOfEpisodes=30000,stepsPerEpisode=12,epsilon=1,annealingConstant=0.98,annealAfter=250);
 
 ############## end block ####################
 
 #ql1=qLearning(learningRate=0.001,decayRate=0.9,numOfEpisodes=24000,stepsPerEpisode=12,epsilon=1,annealingConstant=0.98,annealAfter=200);
 #ql2=qLearning(learningRate=0.001,decayRate=0.9,numOfEpisodes=30000,stepsPerEpisode=12,epsilon=1,annealingConstant=0.98,annealAfter=250);
 #ql1.compareWith([ql2])
-ql2=qLearning(learningRate=0.001,decayRate=0.7,numOfEpisodes=30000,stepsPerEpisode=12,epsilon=1,annealingConstant=0.98,annealAfter=250);
+#ql2=qLearning(learningRate=0.001,decayRate=0.7,numOfEpisodes=30000,stepsPerEpisode=12,epsilon=1,annealingConstant=0.98,annealAfter=250);
 #print(ql1.states[0])
+#ql1.comparePerformance(steps=24, oper_upd_interval=6, bus_index_shunt=1, bus_index_voltage=1, line_index=1)
+ql7=qLearning(learningRate=0.001,decayRate=0.9,numOfEpisodes=288000,stepsPerEpisode=1,epsilon=1,annealingConstant=0.98,annealAfter=2400);
+#ql1.train()
+ql7.test()
 #print(ql1.q_table['s1:v_1-1.024_l_0-9;s2:v_0.95-0.974_l_20-29;']);
+
 #print(ql1.getStateFromMeasurements_2([(0.85,9),(0.95,9)]))
 #ql2=qLearning(0.001,0.8,2000,12,1,0.98,150);
 #ql3=qLearning(0.001,0.7,2000,12,1,0.98,150);
 #ql2.train();
-ql2.test()
+#ql2.test()
 #ql2.testAllActions()
 # try:
 #     threads = []
