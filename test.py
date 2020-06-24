fce2bda01cfbf7a9ba0f23140bd808f24756f4ae
jplOldeen <joakimoldeen@gmail.com>
2020-06-24 10:25:01 +0200

    Updated the test-file to include the constructors for the 3 RL algorithms chosen for report

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from QLearning import qLearning
 from DQN import DQN
+from TD3 import TD3
 # import threading
 # import time
 # import logging
@@ -66,3 +67,15 @@ from setup import powerGrid_ieee2
 #noRL = powerGrid_ieee2('dqn')
 #noRL.setMode('test')
 #noRL.runNoRL(1)
+
+
+#New prints for RL result:
+#qlres=qLearning(learningRate=0.001,decayRate=0.1,numOfEpisodes=48000,stepsPerEpisode=6,epsilon=1,annealingConstant=0.98,annealAfter=400)
+#dqnres=DQN(2, 0.001, 2000, 64, 0.7, 10000, 12, 1, 0.98, 100, 200, True)
+td3res=TD3(2, lr=0.001, memorySize=2000, batchSize=64,  decayRate=0.1, numOfEpisodes=50000, stepsPerEpisode=12,tau=0.005,policy_freq=2,updateAfter=2)
+td3res.comparePerformance(steps=600, oper_upd_interval=6, bus_index_shunt=1, bus_index_voltage=1, line_index=1, benchmarkFlag=True)
+
+# WINNERS:
+#qlres=qLearning(learningRate=0.001,decayRate=0.1,numOfEpisodes=48000,stepsPerEpisode=6,epsilon=1,annealingConstant=0.98,annealAfter=400)
+#dqnres=DQN(2, 0.001, 2000, 64, 0.7, 10000, 12, 1, 0.98, 100, 200, True)
+#td3res=TD3(2, lr=0.001, memorySize=2000, batchSize=64,  decayRate=0.1, numOfEpisodes=50000, stepsPerEpisode=12,tau=0.005,policy_freq=2,updateAfter=2)
